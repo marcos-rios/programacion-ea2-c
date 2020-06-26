3ef602601836c508d66dbe5e955d622f84f0bb0e
Marcos <marcosarios@gmail.com>
2020-06-26 22:10:54 +0100

    Initial commit

---
+++ funciones.c
@@ -0,0 +1,87 @@
+/**//* 22.333.444-PEREZ_DEL_RIO,JuanManuel-(07-2299) *//**/
+
+
+/**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**/
+/**//**//* CUALQUIER INCLUDE DE BIBLIOTECA QUE NECESITE, HÁGALO ACÁ   *//**//**/
+
+
+/**//**//* CUALQUIER INCLUDE DE BIBLIOTECA QUE NECESITE, HÁGALO ACÁ   *//**//**/
+/**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**/
+#include "funciones.h"
+/**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**/
+/**//**//* ACÁ DEBE DESARROLLAR LAS FUNCIONES Y PRIMITIVAS PEDIDAS    *//**//**/
+/**//**//* ADEMÁS DE CUALQUIER OTRA FUNCIÓN QUE SE REQUIERA           *//**//**/
+
+
+
+/**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**/
+/**//* FUNCIONES Y PRIMITIVAS A DESARROLLAR                               *//**/
+/**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**/
+/**//* para la información                                                *//**/
+/**
+void mostrarMovim_MIO(const void *d, FILE *fp)
+{
+
+}
+ **/
+/**
+int compararMovim_MIO(const void *d1, const void *d2)
+{
+
+}
+ **/
+/**
+int esCtaCte002_MIO(const void *d)
+{
+
+}
+ **/
+/**
+int acumularMoviSaldo_MIO(void **dest, unsigned *tamDest,
+                      const void *ori, unsigned tamOri)
+{
+
+}
+ **/
+/**
+void mostrarTotal_MIO(const void *d, FILE *fp)
+{
+
+}
+ **/
+
+/**//* para el TDA LISTA                                                  *//**/
+/**
+int mostrarLista_MIO(const tLista *p,
+                     void (*mostrar)(const void *, FILE *), FILE *fp)
+{
+
+}
+ **/
+/**
+void ordenarLista_MIO(tLista *p, int (*comparar)(const void *, const void *))
+{
+
+}
+ **/
+/**
+int eliminarMostrarYMostrarSubTot_MIO(tLista *p, FILE *fpPant,
+                                      int comparar(const void *, const void *),
+                                      int comparar2(const void *d),
+                                      int acumular(void **, unsigned *,
+                                                   const void *, unsigned),
+                                      void mostrar(const void *, FILE *),
+                                      void mostrar2(const void *, FILE *))
+{
+
+}
+ **/
+/**
+int  vaciarListaYMostrar_MIO(tLista *p,
+                             void (*mostrar)(const void *, FILE *), FILE *fp)
+{
+
+}
+ **/
+/**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**/
+
