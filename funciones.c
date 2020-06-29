fe633eff993d1552c3923ed8162a2b681a066f70
Marcos <marcosarios@gmail.com>
2020-06-29 03:47:47 +0100

    add first approach for ordering and improve display function

--- funciones.c
+++ funciones.c
@@ -1,9 +1,10 @@
-/**//* 22.333.444-PEREZ_DEL_RIO,JuanManuel-(07-2299) *//**/
+/**//* 35.588.676-RIOS,MarcosAdrian-(01-2964) *//**/
 
 
 /**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**/
 /**//**//* CUALQUIER INCLUDE DE BIBLIOTECA QUE NECESITE, HÁGALO ACÁ   *//**//**/
-
+#include <string.h>
+#include <stdlib.h>
 
 /**//**//* CUALQUIER INCLUDE DE BIBLIOTECA QUE NECESITE, HÁGALO ACÁ   *//**//**/
 /**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**/
@@ -11,7 +12,6 @@
 /**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**/
 /**//**//* ACÁ DEBE DESARROLLAR LAS FUNCIONES Y PRIMITIVAS PEDIDAS    *//**//**/
 /**//**//* ADEMÁS DE CUALQUIER OTRA FUNCIÓN QUE SE REQUIERA           *//**//**/
-void mostrarMovimTxt(const tMovi *d, FILE *fp);
 
 
 /**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**/
@@ -21,48 +21,43 @@ void mostrarMovimTxt(const tMovi *d, FILE *fp);
 
 void mostrarMovim_MIO(const void *d, FILE *fp)
 {
-    mostrarMovimTxt(d, fp);
-}
-
-void mostrarMovimTxt(const tMovi *d, FILE *fp)
-{
-    if(d)
+    const tMovi *mov = d;
+    if(mov)
         fprintf(fp,
                 "%-*s %*.02f\n",
-                (int)sizeof(d->ctaCte), d->ctaCte,
-                (int)sizeof(d->saldo), d->saldo);
+                (int)sizeof(mov->ctaCte), mov->ctaCte,
+                (int)sizeof(mov->saldo), mov->saldo);
     else
         fprintf(fp,
                 "%-*s %*s\n",
-                (int)sizeof(d->ctaCte), "Nro Cuenta Banc",
-                (int)sizeof(d->saldo), "Importe");
+                (int)sizeof(mov->ctaCte), "Nro Cuenta Banc",
+                (int)sizeof(mov->saldo), "Importe");
 }
 
-/**
 int compararMovim_MIO(const void *d1, const void *d2)
 {
-
+    return strcmp(((tMovi *)d1)->ctaCte, ((tMovi *)d2)->ctaCte);
 }
- **/
-/**
-int esCtaCte002_MIO(const void *d)
-{
 
-}
- **/
-/**
-int acumularMoviSaldo_MIO(void **dest, unsigned *tamDest,
-                      const void *ori, unsigned tamOri)
-{
 
-}
- **/
-/**
-void mostrarTotal_MIO(const void *d, FILE *fp)
-{
+//int esCtaCte002_MIO(const void *d)
+//{
+//    return 1;
+//}
+//
+//
+//int acumularMoviSaldo_MIO(void **dest, unsigned *tamDest,
+//                      const void *ori, unsigned tamOri)
+//{
+//
+//}
+//
+//
+//void mostrarTotal_MIO(const void *d, FILE *fp)
+//{
+//
+//}
 
-}
- **/
 
 /**//* para el TDA LISTA                                                  *//**/
 int mostrarLista_MIO(const tLista *p,
@@ -80,30 +75,74 @@ int mostrarLista_MIO(const tLista *p,
     return cant;
 }
 
-/**
+// tLista *p = direccion de un puntero a nodo
 void ordenarLista_MIO(tLista *p, int (*comparar)(const void *, const void *))
 {
+    if(*p == NULL)
+        return;
 
+    tLista *inicio = p;
+    int primero = 1;
+    while((*p)->sig)
+    {
+        tNodo *q = *p;
+        tNodo *minAct = q;
+        tNodo *preMin = NULL;
+        while(q->sig)
+        {
+            printf("Q actual: %s\n", ((tMovi *)q->info)->ctaCte);
+            if(comparar(q->sig->info, minAct->info) < 0)
+            {
+                preMin = q;
+                minAct = q->sig;
+                if(primero)
+                {
+                    inicio = &(q->sig);
+                    primero = 0;
+                }
+            }
+            q = q->sig;
+        }
+        puts("\n");
+        if(preMin)
+        {
+            preMin->sig = minAct->sig;
+            minAct->sig = *p;
+        }
+        p = &(*p)->sig;
+    }
+    *p = *inicio;
 }
- **/
-/**
-int eliminarMostrarYMostrarSubTot_MIO(tLista *p, FILE *fpPant,
-                                      int comparar(const void *, const void *),
-                                      int comparar2(const void *d),
-                                      int acumular(void **, unsigned *,
-                                                   const void *, unsigned),
-                                      void mostrar(const void *, FILE *),
-                                      void mostrar2(const void *, FILE *))
-{
 
-}
- **/
-/**
+
+//int eliminarMostrarYMostrarSubTot_MIO(tLista *p, FILE *fpPant,
+//                                      int comparar(const void *, const void *),
+//                                      int comparar2(const void *d),
+//                                      int acumular(void **, unsigned *,
+//                                                   const void *, unsigned),
+//                                      void mostrar(const void *, FILE *),
+//                                      void mostrar2(const void *, FILE *))
+//{
+//
+//}
+//
 int  vaciarListaYMostrar_MIO(tLista *p,
                              void (*mostrar)(const void *, FILE *), FILE *fp)
 {
-
+    int cant = 0;
+    while(*p)
+    {
+        tNodo *aux = *p;
+        cant++;
+        *p = aux->sig;
+        if(mostrar && fp)
+        {
+            mostrar(aux->info, fp);
+        }
+        free(aux->info);
+        free(aux);
+    }
+    return cant;
 }
- **/
 /**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**/
 
--- main.c
+++ main.c
@@ -23,30 +23,30 @@ int main()
             cant);
 
 
-//    fprintf(fpPant, "Ordenando por número de cuenta.\n");
+    fprintf(fpPant, "Ordenando por número de cuenta.\n");
 //    ordenarLista(&lista, compararMovim);
-//    ordenarLista_MIO(&lista, compararMovim_MIO);
-//
+    ordenarLista_MIO(&lista, compararMovim_MIO);
+
 //    if(cant != mostrarLista(&lista, mostrarMovim, fpPant))
-//    if(cant != mostrarLista_MIO(&lista, mostrarMovim_MIO, fpPant))
-//    {
-//        fprintf(fpPant, "Ups!!! - Faltan cuentas.\n");
-//    }
-//    else
-//    {
-//        fprintf(fpPant,
-//                "Se mostraron %d movimientos bancarios en la lista.\n\n",
-//                cant);
-//    }
-//
-//
-//    fprintf(fpPant, "Eliminando y mostrando los movimientos de CtaCte.\n");
-//    cantElim = eliminarMostrarYMostrarSubTot(&lista, fpPant,
-//                                             compararMovim,
-//                                             esCtaCte002,
-//                                             acumularMoviSaldo,
-//                                             mostrarMovim,
-//                                             mostrarTotal);
+    if(cant != mostrarLista_MIO(&lista, mostrarMovim_MIO, fpPant))
+    {
+        fprintf(fpPant, "Ups!!! - Faltan cuentas.\n");
+    }
+    else
+    {
+        fprintf(fpPant,
+                "Se mostraron %d movimientos bancarios en la lista.\n\n",
+                cant);
+    }
+
+
+    fprintf(fpPant, "Eliminando y mostrando los movimientos de CtaCte.\n");
+    cantElim = eliminarMostrarYMostrarSubTot(&lista, fpPant,
+                                             compararMovim,
+                                             esCtaCte002,
+                                             acumularMoviSaldo,
+                                             mostrarMovim,
+                                             mostrarTotal);
 //    cantElim = eliminarMostrarYMostrarSubTot_MIO(&lista, fpPant,
 //                                                 compararMovim_MIO,
 //                                                 esCtaCte002_MIO,
@@ -54,11 +54,11 @@ int main()
 //                                                 mostrarMovim_MIO,
 //                                                 mostrarTotal_MIO);
 //    fprintf(fpPant, "Se eliminaron %d nodos de CtaCte.\n\n", cantElim);
-//
-//    fprintf(fpPant, "Vaciando la lista y mostrando las cuentas remanentes.\n");
+
+    fprintf(fpPant, "Vaciando la lista y mostrando las cuentas remanentes.\n");
 //    cant = vaciarListaYMostrar(&lista, mostrarMovim, fpPant);
-//    cant = vaciarListaYMostrar_MIO(&lista, mostrarMovim_MIO, fpPant);
-//    fprintf(fpPant, "Se eliminaron y mostraron %d nodos de la lista", cant);
+    cant = vaciarListaYMostrar_MIO(&lista, mostrarMovim_MIO, fpPant);
+    fprintf(fpPant, "Se eliminaron y mostraron %d nodos de la lista", cant);
 
     if(fpPant != stdout && fpPant != stderr && fpPant != NULL)
     {
